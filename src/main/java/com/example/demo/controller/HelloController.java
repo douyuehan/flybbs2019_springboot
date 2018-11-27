fa7ca6a3c9a9ed58bfd3156d151fbbd3d4e1b517
your name <your email>
2018-11-27 13:37:56 +0800

    jsp

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.LearnResouce;
 import com.example.demo.mapper.LearnMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: @我没有三颗心脏
  * @create: 2018-05-08-下午 16:46
  */
-@RestController
+@Controller
 public class HelloController {
     @Autowired
     LearnMapper learnMapper;
     @RequestMapping("/hello")
-    public String hello() {
-        LearnResouce learnResouce = learnMapper.queryLearnResouceById(999L);
+    public String hello(Model model) {
+        LearnResouce learnResouce = learnMapper.queryLearnResouceById(1001L);
+        model.addAttribute("learn",learnResouce);
         System.out.println(learnResouce);
-        return learnResouce.toString();
+        return "index";
     }
 }
